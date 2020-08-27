e506834d3835a28c642b618297c0c1bfa4b602eb
Tristan Thompson <tristan15thompson@gmail.com>
2020-08-27 14:43:35 -0700

    Calculator added menu and Calculation function

--- Calculator/calc.py
+++ Calculator/calc.py
@@ -4,21 +4,45 @@
 # one part will do a calculation based on two numbers and an operator
 # one calculation for finding BMI (body mass index)
 
+import os
+
+
 def performCalculation():
-    print("hi")
+    print("\n\nWelcome to my Calculator, in order for this to function, you may use one of the four operators: + - * /\n" +
+          "So, tell me what you would like me to calculate")
+    firstNum = int(input("First number: "))
+    operator = input("Operator: ")
+    secondNum = int(input("Second number: "))
+
+    if (operator == "+"):
+        answer = firstNum + secondNum
+    elif (operator == "-"):
+        answer = firstNum - secondNum
+    elif (operator == "*"):
+        answer = firstNum * secondNum
+    elif (operator == "/"):
+    answer = firstNum / secondNum
+    else:
+        print("oh no! you cannot use that operator")
+        os.system("clear")
+        startingMenu()
+    print(answer)
+
 
 def calculateBMI():
     print("hello")
 
-print("Hello, Welcome to my Calculation Program!\n" +
-    "What would you like to do?\n\n")
 
-selection = input("1) Perform a calculation\n" +
-    "2) Calculate your BMI\n")
+def startingMenu():
+    print("Hello, Welcome to my Calculation Program!\n" +
+          "What would you like to do?\n\n")
+    selection = input("1) Perform a calculation\n" +
+                      "2) Calculate your BMI\n")
+    if (selection == "1"):
+        performCalculation()
+    elif (selection == "2"):
+        calculateBMI()
+    else:
+        exit()
 
-if (selection == "1"):
-    performCalculation()
-elif (selection == "2"):
-     calculateBMI()
-else: exit()
-
+startingMenu()
