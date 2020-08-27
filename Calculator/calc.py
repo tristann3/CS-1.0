d84d25cf2dd017b52f5a47541a9252afd65cbcfc
Tristan Thompson <tristan15thompson@gmail.com>
2020-08-27 14:07:17 -0700

    finished dataflow

--- Calculator/calc.py
+++ Calculator/calc.py
@@ -1,2 +1,24 @@
 # Custom Calculator by Tristan Thompson
 # 8/27/20
+# This will have two functions, a small menu with two calculations
+# one part will do a calculation based on two numbers and an operator
+# one calculation for finding BMI (body mass index)
+
+def performCalculation():
+    print("hi")
+
+def calculateBMI():
+    print("hello")
+
+print("Hello, Welcome to my Calculation Program!\n" +
+    "What would you like to do?\n\n")
+
+selection = input("1) Perform a calculation\n" +
+    "2) Calculate your BMI\n")
+
+if (selection == "1"):
+    performCalculation()
+elif (selection == "2"):
+     calculateBMI()
+else: exit()
+
