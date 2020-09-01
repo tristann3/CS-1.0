59509150bc9ac32cc14d17fa69431aa261f65ac6
Tristan Thompson <tristan15thompson@gmail.com>
2020-09-01 11:48:36 -0700

    functions.py completed

---
+++ functions-practice/functions.py
@@ -0,0 +1,31 @@
+# Functions practice by Tristan Thomspon 10/1
+
+def area(width, height):
+    result = width * height
+    return result
+
+result = area(5,6)
+print(result)
+result = area(10,90)
+print(result)
+result = area(9,2)
+print(result)
+
+def subtract(num1,num2):
+    result = num1 - num2
+    return result
+
+result = subtract(10,5)
+print(result)
+result = subtract(10,90)
+print(result)
+
+def divide(num1,num2):
+    result = num1/num2
+    return result
+
+def add(num1,num2):
+    return num1+num2
+
+print(add(1,1))
+print(add(1,10))
