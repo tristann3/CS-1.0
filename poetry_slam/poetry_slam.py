99b5c9f85fe0856f02dde215175934443b864ca4
Tristan <tristan15thompson@gmail.com>
2020-09-24 17:32:17 -0700

    lines_printed_backwards functioning, added comments

--- poetry_slam/poetry_slam.py
+++ poetry_slam/poetry_slam.py
@@ -1,14 +1,17 @@
 
-
+#this will read all the lines of the file and return a list of line strings
 def get_file_lines(filename):
     return open(filename, "r").readlines()
 
- #print lines in reverse and include line numbers
-def print_lines_backwards(lines_list):
+# this will loop through my list reversed, print the current length of the list along with the line string
+# then it will remove the last element to keep the line number accurate
+def lines_printed_backwards(lines_list):
+    for line in reversed(lines_list):
+        print(f"{len(lines_list)} {line}")
+        lines_list.pop()
     return
 
 file = "poem.txt"
 file_lines = get_file_lines(file)
-for line in file_lines:
-    print(line)
 
+lines_printed_backwards(file_lines)
