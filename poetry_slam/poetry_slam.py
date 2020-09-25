faeb987a8a114c5a34938d74c8df213d6b5b6314
Tristan <tristan15thompson@gmail.com>
2020-09-24 18:53:27 -0700

    lines_printed_custom functioning

--- poetry_slam/poetry_slam.py
+++ poetry_slam/poetry_slam.py
@@ -19,10 +19,26 @@ def lines_printed_random(lines_list):
         rand_line = random.randint(1,total_lines)
         print(lines_list[rand_line])
 
+#this will print my list in a first-last pattern so it will print line 1, then 18, 2, 17 and so on...until it reaches
+def lines_printed_custom(lines_list):
+    small_line = 0
+    big_line = len(lines_list) - 1
+    for line in lines_list:
+        print(f"{small_line + 1} {lines_list[small_line]}")
+        print(f"{big_line + 1} {lines_list[big_line]}")
+        small_line += 1
+        big_line -= 1
+        if (big_line == 9): # this means the loop ran 9 times which will means the file is complete
+            break
+
+        
+
 
 file = "poem.txt"
 file_lines = get_file_lines(file)
 
-lines_printed_backwards(file_lines)
+# lines_printed_backwards(file_lines)
 
-lines_printed_random(file_lines)
+# lines_printed_random(file_lines)
+
+lines_printed_custom(file_lines)
