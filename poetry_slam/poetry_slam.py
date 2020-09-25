ac20abb0b3733a33cb011070bb786025bb4c125b
Tristan <tristan15thompson@gmail.com>
2020-09-24 17:51:32 -0700

    lines_printed_random functioning

--- poetry_slam/poetry_slam.py
+++ poetry_slam/poetry_slam.py
@@ -1,17 +1,28 @@
+import random
 
 #this will read all the lines of the file and return a list of line strings
 def get_file_lines(filename):
     return open(filename, "r").readlines()
 
 # this will loop through my list reversed, print the current length of the list along with the line string
-# then it will remove the last element to keep the line number accurate
+# then it will decrease the line counter
 def lines_printed_backwards(lines_list):
+    total_lines = len(lines_list)
     for line in reversed(lines_list):
-        print(f"{len(lines_list)} {line}")
-        lines_list.pop()
-    return
+        print(f"{total_lines} {line}")
+        total_lines -= 1
+
+# this will loop through my list randomly
+def lines_printed_random(lines_list):
+    total_lines = len(lines_list) - 1 #since indexing starts at zero, we must decrease the total linesfor out of bounds errors
+    for line in lines_list:
+        rand_line = random.randint(1,total_lines)
+        print(lines_list[rand_line])
+
 
 file = "poem.txt"
 file_lines = get_file_lines(file)
 
 lines_printed_backwards(file_lines)
+
+lines_printed_random(file_lines)
