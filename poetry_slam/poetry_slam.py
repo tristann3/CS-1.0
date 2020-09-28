1a28149b63380e904e2945387d5f1d10f6ecab20
Tristan <tristan15thompson@gmail.com>
2020-09-28 15:02:31 -0700

    last revision, added comments and formatting

--- poetry_slam/poetry_slam.py
+++ poetry_slam/poetry_slam.py
@@ -19,18 +19,19 @@ def lines_printed_random(lines_list):
         rand_line = random.randint(1,total_lines)
         print(lines_list[rand_line])
 
-#this will print my list in a first-last pattern so it will print line 1, then 18, 2, 17 and so on...until it reaches
+#this will print my list in a first-last pattern so it will print line 1, then 18, 2, 17 and so on...until it reaches the end of the poem
 def lines_printed_custom(lines_list):
-    for x in range(int(len(lines_list)/2)):
+    nl = '\n'
+    for x in range(int(len(lines_list)/2)): # this is the length of the list divided by two because we are printing 2 lines per iteration
         print(f"{x+1} {lines_list[x]}")
-        print(f"{len(lines_list) - x} {lines_list[len(lines_list) - x - 1]}" )
+        print(f"{len(lines_list) - x} {lines_list[len(lines_list) - x - 1]} {nl}") # by referencing the index of the length of the list minus the current iteration minus 1, we can access the bottom of the poem recursively(?)
 
 
 file = "poem.txt"
 file_lines = get_file_lines(file)
 
-lines_printed_backwards(file_lines)
+# lines_printed_backwards(file_lines)
 
-lines_printed_random(file_lines)
+# lines_printed_random(file_lines)
 
 lines_printed_custom(file_lines)
