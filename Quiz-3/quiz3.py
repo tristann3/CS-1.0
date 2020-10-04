3dc4c40ffb5f3eee14d2a0b48a5822c8cd88964f
Tristan <tristan15thompson@gmail.com>
2020-10-03 17:19:26 -0700

    quiz 3

---
+++ Quiz-3/quiz3.py
@@ -0,0 +1,16 @@
+hats = {
+    "snapback": 10,
+    "beanie": 5,
+    "baseballcap": 3
+}
+
+# adds one weird top hat to my hats dictionary
+hats["weird top hat"] = 1
+
+# adds one more snapback to my hats dictionary
+hats["snapback"] += 1
+
+# deletes the 'weird top hat from my dictionary, referencing by index
+del hats["weird top hat"]
+print(hats)
+
